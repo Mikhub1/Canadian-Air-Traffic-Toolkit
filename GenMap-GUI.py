467166df44125d5bff0716b94d6a5d167a90902c
Iryna Borshchova <58992009+spydoctor@users.noreply.github.com>
2025-11-18 06:41:06 -0500

    Update GenMap-GUI.py

--- GenMap-GUI.py
+++ GenMap-GUI.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 National Research Council Canada
+#Copyright (c) 2025 National Research Council Canada
 import sys
 import os
 import pandas as pd
@@ -541,6 +541,7 @@ clicked1.trace('w',ch)
 clicked2.trace('w',ch2)
 
 w.mainloop()
+
 
 
 
